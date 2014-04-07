5a7868a4fbe05745d8f91a33eecf349c1c1c0c76
Lucas Viola <lucasmatzenbacher@gmail.com>
2014-04-07 08:20:28 -0300

    updated original framework files

---
+++ view/_masterpage/header.php
@@ -0,0 +1,15 @@
+<?php
+	global $pilarMVC;
+?>
+<html>
+	<head>
+		<title></title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="/css/style.css" />
+		<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+		
+	</head>
+	<body>
+		<header>
+		</header>
+
